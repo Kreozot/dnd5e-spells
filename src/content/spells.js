cb641b12d735d2837995ace75e55b2195341eaa6
Sergey Sharov <sssharov@domclick.ru>
2020-08-03 16:05:51 +0300

    Components and hints formatting

--- src/content/spells.js
+++ src/content/spells.js
@@ -1,20 +1,23 @@
 import { formatText } from 'common/format';
 import data from 'content/spells.yaml';
 
-function format(text) {
-  let result = formatText(text);
-
-  return result + '\n';
-}
-
 function formatSpells(data) {
   return data.map((spell) => {
-    spell.description = format(spell.description);
+    spell.description = formatText(spell.description) + '\n';
     if (spell.atHigherLevels) {
-      spell.atHigherLevels = format(spell.atHigherLevels);
+      spell.atHigherLevels = formatText(spell.atHigherLevels) + '\n';
     }
     if (spell.levelUpgrades) {
-      spell.levelUpgrades = format(spell.levelUpgrades);
+      spell.levelUpgrades = formatText(spell.levelUpgrades) + '\n';
+    }
+    if (typeof spell.components.M === 'string') {
+      spell.components.M = formatText(spell.components.M);
+    }
+    if (typeof spell.components.M === 'object') {
+      spell.components.M = spell.components.M.map((component) => formatText(component));
+    }
+    if (typeof spell.castingTime.hint === 'string') {
+      spell.castingTime.hint = formatText(spell.castingTime.hint);
     }
 
     return spell;
