4490c0afceee06d7daa152fbac5f3b0dcde98918
Sergey Sharov <sssharov@domclick.ru>
2020-08-03 10:34:29 +0300

    Dice roll highlight

--- src/content/spells.js
+++ src/content/spells.js
@@ -1,14 +1,30 @@
 import { formatText } from 'common/format';
 import data from 'content/spells.yaml';
 
+const DICE_REGEXP = /([0-9]*d[0-9]+s?)/gm;
+
+function highlightDices(text) {
+  return text.replace(DICE_REGEXP, '<strong>$1</strong>');
+}
+
+function format(text) {
+  let result = text;
+
+  result = highlightDices(result);
+
+  result = formatText(result);
+
+  return result + '\n';
+}
+
 function formatSpells(data) {
   return data.map((spell) => {
-    spell.description = formatText(spell.description) + '\n';
+    spell.description = format(spell.description);
     if (spell.atHigherLevels) {
-      spell.atHigherLevels = formatText(spell.atHigherLevels) + '\n';
+      spell.atHigherLevels = format(spell.atHigherLevels);
     }
     if (spell.levelUpgrades) {
-      spell.levelUpgrades = formatText(spell.levelUpgrades) + '\n';
+      spell.levelUpgrades = format(spell.levelUpgrades);
     }
 
     return spell;
