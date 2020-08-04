a62971bb2c9f96f25db84ecbc571169c464300a6
Sergey Sharov <sssharov@domclick.ru>
2020-08-04 18:04:24 +0300

    Header removed

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,26 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 import 'react-tippy/dist/tippy.css'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>
       </div>
