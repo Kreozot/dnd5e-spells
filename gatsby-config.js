516918c47350f39f4a17b5abdeccebbbe7ed0a71
Sergey Sharov <sergey_sharov@smart-x.net>
2022-10-08 16:15:59 +0200

    Format spells at build time

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        name: 'content',
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         name: 'images',
         path: `${__dirname}/src/images`,
       },
